d7963aed7193694be86e308d99364a2ab4fa5bea
anszdev <anszdev@outlook.com>
2025-05-17 00:45:07 -0600

    feat: add landing page components and structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+import { Container } from "@components/common/Container";
+import { Video } from "@components/landing/Video";
+import { Header } from "@components/landing/Header";
+import { Navbar } from "@components/landing/Navbar";
+
 export default function Home() {
-  return <h1>Invit🪅Arte</h1>;
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <section className="mt-60">
+        <Container>
+          <div className="bg-thin p-5 rounded-4xl">
+            <Video />
+          </div>
+        </Container>
+      </section>
+    </>
+  );
 }
